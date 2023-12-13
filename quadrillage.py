e4213f0b910bc925d8e67344c613da884c463c89
Polo <a.rahmy@proton.me>
2023-12-13 09:19:57 +0100

    First Upload

--- quadrillage.py
+++ quadrillage.py
@@ -1,33 +1,42 @@
-import turtle
+from turtle import *
+from random import *
 
-# Fonction pour dessiner une ligne horizontale
-def draw_horizontal_line(x, y):
-    turtle.penup()
-    turtle.goto(x, y)
-    turtle.pendown()
-    turtle.forward(cell_size * 3)
-
-# Fonction pour dessiner une ligne verticale
-def draw_vertical_line(x, y):
-    turtle.penup()
-    turtle.goto(x, y)
-    turtle.setheading(270)  # Orientation vers le bas
-    turtle.pendown()
-    turtle.forward(cell_size * 3)
-
-# Initialisation de Turtle
-turtle.speed(0)  # Vitesse maximale
-
-# Taille de la cellule
-cell_size = 100
-
-# Dessiner le quadrillage
-for i in range(1, 3):
-    draw_horizontal_line(-cell_size, i * cell_size)
-    draw_vertical_line(i * cell_size, -cell_size)
-
-# Dessiner la dernière ligne verticale
-draw_vertical_line(2 * cell_size, -cell_size)
-
-# Attendre que l'utilisateur ferme la fenêtre
-turtle.done()
+speed(0)
+title("Quadrillage Morpion")
+Taille=600
+Marge =30
+setup(Taille,Taille+100)
+Case=(Taille-2*Marge)//3
+up()
+goto(Marge-Taille//2,30-Taille//2)
+down()
+up()
+goto(0,30-Taille//2)
+down()
+up()
+color('black')
+goto(Marge-Taille//2,Taille//2-Marge+50)
+width(3)
+for i in range(4):
+    down()
+    forward(3*Case)
+    up()
+    backward(3*Case)
+    right(90)
+    forward(Case)
+    left(90)
+left(90)
+forward(Case)
+for i in range(4):
+    down()
+    forward(3*Case)
+    up()
+    backward(3*Case)
+    right(90)
+    forward(Case)
+    left(90)
+left(90)
+forward(4*Case-30)
+right(90)
+forward(2*Case)
+mainloop()
