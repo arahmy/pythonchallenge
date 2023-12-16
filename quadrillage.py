2a7a425dddd8c7a111aa3520df39595e2804f258
Polo <a.rahmy@proton.me>
2023-12-16 14:01:33 +0100

    Modification de la base - Prise en compte des paramètres de l'utilisateur

--- quadrillage.py
+++ quadrillage.py
@@ -1,42 +1,49 @@
 from turtle import *
-from random import *
+
+def Quadrillage(x, y, taille, nb):
+    up()
+    goto(x - taille / 2, y - taille / 2)
+    down()
+    up()
+    goto(x, y - taille / 2)
+    down()
+    up()
+    color('black')
+    width(3)
+    for i in range(nb + 1):
+        down()
+        forward(nb * taille)
+        up()
+        backward(nb * taille)
+        right(90)
+        forward(taille)
+        left(90)
+    left(90)
+    forward(taille)
+    for i in range(nb + 1):
+        down()
+        forward(nb * taille)
+        up()
+        backward(nb * taille)
+        right(90)
+        forward(taille)
+        left(90)
+    left(90)
+    forward((nb + 1) * taille)
+    right(90)
+    forward((nb - 1) * taille / 2)
+
+# Input - Prise en compte des paramètres
+x = float(input("Entrez la coordonnée x : "))
+y = float(input("Entrez la coordonnée y : "))
+taille = float(input("Entrez la taille de chaque case (La taille 10 est recommandée) : "))
+nb_cases = int(input("Entrez le nombre de cases par côté : "))
 
 speed(0)
 title("Quadrillage - Morpion")
-Taille=600
-Marge =30
-setup(Taille,Taille+100)
-Case=(Taille-2*Marge)//3
-up()
-goto(Marge-Taille//2,30-Taille//2)
-down()
-up()
-goto(0,30-Taille//2)
-down()
-up()
-color('black')
-goto(Marge-Taille//2,Taille//2-Marge+50)
-width(3)
-for i in range(4):
-    down()
-    forward(3*Case)
-    up()
-    backward(3*Case)
-    right(90)
-    forward(Case)
-    left(90)
-left(90)
-forward(Case)
-for i in range(4):
-    down()
-    forward(3*Case)
-    up()
-    backward(3*Case)
-    right(90)
-    forward(Case)
-    left(90)
-left(90)
-forward(4*Case-30)
-right(90)
-forward(2*Case)
+setup(800, 800)
+
+# Utilisation de la fonction Quadrillage avec les paramètres renseignés
+Quadrillage(x, y, taille, nb_cases)
+
 mainloop()
