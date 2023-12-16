df18006da728e42fad11ffe3168c99f70ed4defa
Polo <a.rahmy@proton.me>
2023-12-16 14:23:35 +0100

    Défi 1,2,3 prêt

--- defi_python.py
+++ defi_python.py
@@ -37,68 +37,73 @@ turtle.mainloop() # Laisser la page ouverte
 """
 # Défi 2
 from turtle import *
+from colorama import *
 
-Def Quadrillage(x,y,taille,nb):
-setup(1300,1000)
-speed(1)
-goto(x,y)
-down()
-"""
+def Quadrillage(x, y, taille, nb):
+    up()
+    goto(x - taille / 2, y - taille / 2)
+    down()
+    up()
+    goto(x, y - taille / 2)
+    down()
+    up()
+    color('black')
+    width(3)
+    for i in range(nb + 1):
+        down()
+        forward(nb * taille)
+        up()
+        backward(nb * taille)
+        right(90)
+        forward(taille)
+        left(90)
+    left(90)
+    forward(taille)
+    for i in range(nb + 1):
+        down()
+        forward(nb * taille)
+        up()
+        backward(nb * taille)
+        right(90)
+        forward(taille)
+        left(90)
+    left(90)
+    forward((nb + 1) * taille)
+    right(90)
+    forward((nb - 1) * taille / 2)
 
+# Input - Prise en compte des paramètres
+x = float(input(Fore.RED + "[Défi Python]" + Fore.RESET + " Entrez la coordonnée x : "))
+y = float(input(Fore.RED + "[Défi Python]" + Fore.RESET + " Entrez la coordonnée y : "))
+taille = float(input(Fore.RED + "[Défi Python]" + Fore.RESET + " Entrez la taille de chaque case" + Fore.YELLOW + " (La taille 10 est recommandée) : " + Fore.RESET))
+nb_cases = int(input(Fore.RED + "[Défi Python]" + Fore.RESET + " Entrez le nombre de cases par côté : "))
 
-"""
-setup(1300,1000)
-speed(5)
-down()
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-right(90)
-forward(50)
-right(90)
-forward(50)
-right(90)
-forward(150)
-right(90)
-forward(50)
-right(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(100)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(180)
-forward(100)
-right(90)
-forward(50)
-left(180)
-forward(100)
-left(90)
-forward(50)
-left(90)
-forward(50)
-right(90)
-forward(50)
-right(90)
-forward(50)
-right(90)
-forward(50)
-right(180)
-forward(100)
-left(90)
-forward(50)
-"""
+speed(0)
+title("Quadrillage - Morpion")
+setup(800, 800)
 
+# Appel fonction
+Quadrillage(x, y, taille, nb_cases)
+
+# Laisser la page ouverte
 mainloop()
+
+"""
+"""
+# Défi 3 - Dictionnaire :
+def stat(txt):
+    dic = {}
+    # Parcourir chaque caractère dans le texte
+    for char in txt:
+        # Ignorer les espaces
+        if char != ' ':
+            # Utiliser get() pour obtenir la valeur
+            dic[char] = dic.get(char, 0) + 1
+
+    return dic
+
+# Appel Fonction + Print
+resultat = stat("TAGADA BONBONS ")
+print(resultat)
+"""
+# Défi 4 - Forêt 
